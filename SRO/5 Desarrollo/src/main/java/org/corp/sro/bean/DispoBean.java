dc26e5afd15676293d95faf7694fc9465f5f31b4
Ricardo Díaz <ricardotk002@gmail.com>
2014-07-01 16:50:32 -0500

    disponibilidad updates

---
+++ SRO/5 Desarrollo/src/main/java/org/corp/sro/bean/DispoBean.java
@@ -0,0 +1,128 @@
+package org.corp.sro.bean;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.corp.sro.domain.Reserva;
+import org.corp.sro.service.IReservaService;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+@ManagedBean(name="DispoBean")
+@ViewScoped
+public class DispoBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+    @ManagedProperty("#{ReservaBean}")
+    ReservaBean reservaBean;
+
+	private ScheduleModel eventModel;
+    private String lastCss;
+    private ScheduleEvent event = new DefaultScheduleEvent();
+
+    List<Reserva> reservas;
+
+    public ReservaBean getReservaBean() {
+        return reservaBean;
+    }
+
+    public void setReservaBean(ReservaBean reservaBean) {
+        this.reservaBean = reservaBean;
+    }
+
+    public ScheduleModel getEventModel() {
+		return eventModel;
+	}
+	
+	public void setEventModel(ScheduleModel eventModel) {
+		this.eventModel = eventModel;
+	}
+	
+	public DispoBean(){
+		
+	}
+
+    public IReservaService getReservaService() {
+        return reservaBean.getReservaService();
+    }
+
+    public void setReservaService(IReservaService reservaService) {
+        this.reservaBean.setReservaService(reservaService);
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    @PostConstruct
+	public void init(){	
+		eventModel = new DefaultScheduleModel();
+        refrescarReservasVigentes();
+        reservaBean.setDispoBean(this);
+	}
+
+    public void refrescarReservasVigentes()
+    {
+        eventModel = new DefaultScheduleModel();
+        reservaBean.setReservas(reservaBean.getReservaService().getReservasVigente());
+
+        for(Reserva res:reservaBean.getReservas()){
+            String temp = "Sala "+res.getSala().getCodSala().toString()+"\n"+res.getProcedimiento().getNombre();
+            
+            Integer t= res.getSala().getIdSala()%5;
+            DefaultScheduleEvent dse = new DefaultScheduleEvent(temp,
+                    res.getFechaIni(),res.getFechaFin(),"sala" + t.toString() );
+            dse.setData(res);           
+            eventModel.addEvent(dse);
+        }
+    }
+	
+	public void onDateSelect(SelectEvent selectEvent)
+	{
+		
+	}
+	
+	public void onEventSelect(SelectEvent selectEvent)
+	{
+
+        if(lastCss!="" && lastCss!=null){
+            ((DefaultScheduleEvent) event).setStyleClass(lastCss);
+        }
+
+        event = (ScheduleEvent)selectEvent.getObject();
+        lastCss=event.getStyleClass();
+        ((DefaultScheduleEvent) event).setStyleClass("select");
+
+        reservaBean.setReservaNuevo((Reserva)event.getData());
+
+    }
+	
+	 private Calendar today() {  
+        Calendar calendar = Calendar.getInstance();  
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
+
+        return calendar;
+    }  
+      
+
+      
+
+      
+
+
+}
